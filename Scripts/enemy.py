3e541c6690f6e4c5c13357156ba0b5ac800772f5
EmilChristensen2005 <164002970+EmilChristensen2005@users.noreply.github.com>
2025-03-07 13:18:49 +0000

    Første commit

--- Scripts/enemy.py
+++ Scripts/enemy.py
@@ -2,3 +2,11 @@ import pygame #Her importerer vi pygame
 import math #Her importerer vi math
 pygame.init() #Her initialiserer vi pygame og alle modulerne som nu skulle følge med
 
+
+class Enemy(object):
+
+    def __init__(self, HP, skills, attack, expget):
+        self.HP = HP
+        self.skills = skills
+        self.attack = attack
+        self.expget = expget
